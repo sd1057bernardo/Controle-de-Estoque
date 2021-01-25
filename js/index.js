6bef71781af5c645e08d9e5ca1b03e2af9da11c9
sd1057bernardo <sd1057bernardo@live.com>
2021-01-25 20:21:08 -0300

    funcoes de logica adicionadas

--- js/index.js
+++ js/index.js
@@ -10,7 +10,8 @@ const botoes = [
     "Despachar Produtos",
     "Relatorios",
     "Voltar"
-]
+];
+
 var container = document.querySelector("#container");
 function pages(e){
     container.innerHTML="";
@@ -18,14 +19,18 @@ function pages(e){
         case botoes[0]:
             container.innerHTML+=`<button onclick='pages();'>Voltar</button><br /><h1>${botoes[0]}</h1><hr />${template[0]}${template[2]}${template[3]}`;
             document.querySelector("input[type=button]").value=botoes[0];
+            eventos();
         break;
         case botoes[1]:
             container.innerHTML+=`<button onclick='pages();'>Voltar</button><br /><h1>${botoes[1]}</h1><hr />${template[1]}${template[2]}${template[3]}`;
             document.querySelector("input[type=button]").value=botoes[1];
+            db.ler;
+            eventos();
         break;
         case botoes[2]:
             container.innerHTML+=`<button onclick='pages();'>Voltar</button><br /><h1>${botoes[2]}</h1><hr />${template[1]}${template[2]}${template[3]}`;
             document.querySelector("input[type=button]").value=botoes[2];
+            eventos();
         break;
         case botoes[3]:
             container.innerHTML+=`<button onclick='pages();'>Voltar</button><br /><h1>${botoes[3]}</h1><hr />${template[1]}${template[2]}${template[3]}`;
@@ -33,6 +38,7 @@ function pages(e){
         break;
         default:
             i=0;
+            container.innerHTML+=`<h1>Painel Principal</h1><hr />`;
             for(button of botoes){
                 if(botoes[i] !== botoes[4]){
                     container.innerHTML+=`<button onclick='pages("${botoes[i]}")'>${botoes[i]}</button>`;
@@ -42,7 +48,46 @@ function pages(e){
         break;
     }
 }
+function db(operacao){
+    if(operacao == "ler"){
+        
+    }
+}
+
 function eventos(){
-    
+    var inptselect = document.querySelector("select");
+    var inptnumber = document.querySelector("input[type=number]");
+    var inptbutton = document.querySelector("input[type=button]");
+    var inpttext = document.querySelector("input[type=text]");
+    if(inptnumber){
+        this.addEventListener('change', () => {
+           if(inptnumber.value > 0){
+            inptbutton.disabled = false;
+           } else {
+            inptbutton.disabled = true;
+           }
+        });
+    }
+    inptbutton.addEventListener('click', () => {
+        if(inptbutton.value == botoes[0] && inpttext){
+            if (inpttext.value.length > 8 && confirm(`Cadastrar: ${inpttext.value} X ${inptnumber.value}`)) {
+                
+            } else {
+                alert("Por favor Preencha os campos corretamente.!");
+            }
+        }
+        if(inptbutton.value == botoes[1]){
+            if(confirm(`Receber: ${inptselect.value} X ${inptnumber.value}`)){
+                console.log(`Receber: ${inptselect.value} X ${inptnumber.value}`);
+                
+            }
+        } if(inptbutton.value == botoes[2]){
+            if(confirm(`Despachar: ${inptselect.value} X ${inptnumber.value}`)){
+                console.log(`Despachar: ${inptselect.value} X ${inptnumber.value}`);
+                
+                
+            }
+        }
+    });
 }
 pages();
