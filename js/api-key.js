6f682f33e278552d912f7e3a7ffbcbf46e754c0b
sd1057bernardo <sd1057bernardo@live.com>
2021-01-31 23:01:10 -0300

    Criado Arquivo de configuracoes do Realtime Database

---
+++ js/api-key.js
@@ -0,0 +1,14 @@
+(function (){
+    const firebaseConfig = {
+        apiKey: "AIzaSyCRcp42S4OmRieHdBUIHOOpRc2SNDVnces",
+        authDomain: "estoque-naldo.firebaseapp.com",
+        databaseURL: "https://estoque-naldo-default-rtdb.firebaseio.com",
+        projectId: "estoque-naldo",
+        storageBucket: "estoque-naldo.appspot.com",
+        messagingSenderId: "211399775715",
+        appId: "1:211399775715:web:6a6da55100f60020063cd6",
+        measurementId: "G-262JPP7VV4"
+    };
+    firebase.initializeApp(firebaseConfig);
+        
+})();
