a794dba04f2e55b958f378d2451745679f3dfa4f
sd1057bernardo <sd1057bernardo@live.com>
2021-02-03 02:26:16 -0300

    Criado a seção de relatorios

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ const template = [
     "<input type='text' placeholder='Digite o nome do novo Produto!'>",
     "<select></select>",
     "<input type='number' min='0' placeholder='0'>",
-    "<input type='button' value='Depositar' disabled>"
+    "<input type='button' value='Depositar' disabled>",
+    "<input type='date'>",
+
 ];
 const botoes = [
     "Adicionar Novos Produtos",
@@ -13,6 +15,7 @@ const botoes = [
 ];
 
 var container = document.querySelector("#container");
+var lastup = document.querySelector("#lastupdates");
 var stream = firebase.database();
 var sound = new Audio("barcode.wav");
 var realtimedb = {
@@ -32,6 +35,7 @@ var realtimedb = {
     },
     gravar: (e) => {
         var a = 0;
+        var b = 0;
         stream.ref("produtos").on("value", (snapshot) =>{
             data = snapshot.val();
             for(dados of data){
@@ -42,27 +46,77 @@ var realtimedb = {
                 }
             }
         });
-        if(e[0] == "" ){
-            stream.ref("produtos/"+a).set({
+        stream.ref(`extrato/${tempo().dia}`).on("value", (snapshot) =>{
+            data2 = snapshot.val();
+            for(dados of data2){
+                if(!data2[b]){
+                    b;
+                } else {
+                    b++;
+                }
+            }
+            
+        });
+        if(!e[0]){
+            stream.ref(`produtos/${a}`).set({
                 nome : e[1],
                 quantidade : e[2]
             });
+            
         } else {
-            stream.ref("produtos/"+e[0]).set({
+            stream.ref(`produtos/${e[0]}`).set({
                 nome : e[1],
                 quantidade : e[2]
             });
         }
+        stream.ref(`extrato/${tempo().dia}/${b}/${tempo().hora}`).set({
+            produto : e[1],
+            quantidade : e[2],
+            status : e[3]
+        });
         sound.addEventListener("ended", (e) =>{
             pagina = document.querySelector("input[type=button]").value;
             pages(pagina);
         })
         sound.play();
     },
-    relatorio : () =>{}
+    relatorio : (e) =>{
+        stream.ref(`extrato/${e}`).on("value", (snapshot) =>{
+            data = snapshot.val();
+            i=0;
+            
+            if(data && document.querySelector("input[type=date]")){
+                document.querySelector("input[type=button]").disabled=false;
+            } else {
+                if(document.querySelector("input[type=button]")){
+                    document.querySelector("input[type=button]").disabled=true;
+                }
+            }
+            lastup.innerHTML="<dl>"
+            for(var prop in data){
+            var alvo = data[i];
+            Object.keys(alvo).forEach( (key, index) => {
+                if(alvo[key].status == "Recebido"){
+                    lastup.innerHTML+="<dt style='background-color:MediumSeaGreen'>"+e+" > "+alvo[key].status+"</dt>";
+                }
+                if(alvo[key].status == "Despachado"){
+                    lastup.innerHTML+="<dt style='background-color:Orange'>"+e+" > "+alvo[key].status+"</dt>";
+                }
+                if(alvo[key].status == "Adicionado"){
+                    lastup.innerHTML+="<dt style='background-color:DodgerBlue'>"+e+" > "+alvo[key].status+"</dt>";
+                }
+                lastup.innerHTML+="<dd>"+alvo[key].produto+" X "+alvo[key].quantidade+"</dd>";
+            });
+                i++;
+                
+            }
+            lastup.innerHTML+="</dl>";
+        });
+    }
 }
 function pages(e){
     container.innerHTML="";
+    realtimedb.relatorio(tempo().dia);
     switch(e){
         case botoes[0]:
             container.innerHTML+=`<button onclick='pages();'>Voltar</button><br /><h1>${botoes[0]}</h1><hr />${template[0]}${template[2]}${template[3]}`;
@@ -84,8 +138,9 @@ function pages(e){
             realtimedb.leitura("despachar");
         break;
         case botoes[3]:
-            container.innerHTML+=`<button onclick='pages();'>Voltar</button><br /><h1>${botoes[3]}</h1><hr />${template[1]}${template[2]}${template[3]}`;
+            container.innerHTML+=`<button onclick='pages();'>Voltar</button><br /><h1>${botoes[3]}</h1><hr />${template[4]}${template[3]}`;
             document.querySelector("input[type=button]").value=botoes[3];
+            eventos();
         break;
         default:
             i=0;
@@ -104,6 +159,7 @@ function eventos(){
     var inptnumber = document.querySelector("input[type=number]");
     var inptbutton = document.querySelector("input[type=button]");
     var inpttext = document.querySelector("input[type=text]");
+    var inptdate = document.querySelector("input[type=date]");
     if(inptnumber){
         this.addEventListener('change', () => {
            if(inptnumber.value > 0){
@@ -113,10 +169,16 @@ function eventos(){
            }
         });
     }
+    if(inptdate){
+        inptdate.addEventListener("change", (e) =>{
+            realtimedb.relatorio(inptdate.value);
+        });
+    }
     inptbutton.addEventListener('click', () => {
         if(inptbutton.value == botoes[0] && inpttext){
             if (inpttext.value.length > 8 && confirm(`Cadastrar: ${inpttext.value} X ${inptnumber.value}`)) {
                 produto = ["", inpttext.value, inptnumber.value];
+                produto[3] = "Adicionado";
                 realtimedb.gravar(produto);
                 
             } else {
@@ -128,6 +190,7 @@ function eventos(){
                 console.log(`Receber: ${inptselect.value} X ${inptnumber.value}`);
                 var produto = inptselect.value.split(":");
                 produto[2] = parseInt(produto[2]) + parseInt(inptnumber.value);
+                produto[3] = "Recebido";
                 realtimedb.gravar(produto);
                 
             }
@@ -136,6 +199,7 @@ function eventos(){
                 console.log(`Despachar: ${inptselect.value} X ${inptnumber.value}`);
                 var produto = inptselect.value.split(":");
                 produto[2] = parseInt(produto[2]) - parseInt(inptnumber.value);
+                produto[3] = "Despachado";
                 if(produto[2] >= 0){
                     realtimedb.gravar(produto);
                 } else {
@@ -144,6 +208,19 @@ function eventos(){
             }
         }
     });
-    return ;
+}
+function tempo(){
+today=new Date(),
+h=today.getHours();if(h < 10 ) h = `0${h}`; 
+m=today.getMinutes();if(m < 10 )m = `0${m}`; 
+s=today.getSeconds();if(s < 10 ) s = `0${s}`; 
+d=today.getDate();if(d < 10 ) d = `0${d}`; 
+M=today.getMonth()+1;if(M < 10 ) M = `0${M}`; 
+y=today.getFullYear(); 
+return time ={
+    dia: `${y}-${M}-${d}`,
+    hora: `${h}-${m}-${s}`
+
+}
 }
 pages();
